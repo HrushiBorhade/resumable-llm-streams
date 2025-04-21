3fb6dc182a8b660ae8e35111197c099fb7d39cc8
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-21 23:11:54 +0530

    feat: Add custom local fonts

--- optimized-code/app/page.tsx
+++ optimized-code/app/page.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import ThemeModeToggle from "@/components/theme/theme-toggle";
 import { AnimatedShinyText } from "@/components/magicui/animated-shiny-text";
+import useSession from "@/hooks/use-session";
 
 export default function Home() {
+  const { session, regenerateSessionId, clearSessionId } = useSession();
   return (
     <div className="bg-background w-full h-screen flex items-center justify-center">
       <div className="container w-full flex flex-col justify-center items-center gap-3 mx-auto p-4">
-        <AnimatedShinyText className="text-4xl font-bold font-mono">
-          resilient-llm-streams
+        <AnimatedShinyText className="text-4xl font-medium  -tracking-[1px] leading-[1.2] overflow-hidden font-serif">
+          {"Resilient llm streams".split("").map((letter, index) => {
+            return (
+              <span
+                key={index}
+                className={`animate-letter ${index>=14 && "italic"}`}
+                style={{ animationDelay: `${index * 0.05}s` }}
+              >
+                {letter}
+              </span>
+            );
+          })}
         </AnimatedShinyText>
       </div>
-
 
       {/* theme mode toggle */}
       <div className="absolute bottom-3 right-3">
@@ -20,12 +33,13 @@ export default function Home() {
       {/* builder info */}
       <div className="absolute left-1/2 bottom-2 -translate-x-1/2">
         <div className="flex items-center justify-center gap-0.5">
+      
           <Link
             href="https://x.com/BorhadeHrushi"
             target="_blank"
             className="group relative"
           >
-            <p className="font-kalam text-lg font-light">~ ऋषी</p>
+            <p className="font-kalam text-sm font-light italic">~ऋषी</p>
             <span className="absolute inset-x-0 bottom-[2px] h-px bg-gradient-to-b from-[#d1d5db] via-[#6b7280] to-[#374151] opacity-0 transition group-hover:opacity-100"></span>
           </Link>
         </div>
--- optimized-code/components/magicui/animated-shiny-text.tsx
+++ optimized-code/components/magicui/animated-shiny-text.tsx
@@ -21,13 +21,13 @@ export const AnimatedShinyText: FC<AnimatedShinyTextProps> = ({
         } as CSSProperties
       }
       className={cn(
-        "mx-auto max-w-lg text-neutral-600/70 dark:text-neutral-400/70",
+        "mx-auto max-w-lg text-neutral-800/50 dark:text-neutral-200/70 ",
 
         // Shine effect
-        "animate-shiny-text bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shiny-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]",
+        "animate-shiny-text  duration-[3000] bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shiny-width)_100%] [transition:background-position_0.5s_cubic-bezier(.6,.6,0,1)_infinite]",
 
         // Shine gradient
-        "bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent  dark:via-white/80",
+        "bg-gradient-to-r from-transparent via-black/30 via-50% to-transparent  dark:via-white",
 
         className,
       )}
